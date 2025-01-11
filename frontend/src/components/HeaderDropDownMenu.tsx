1a8aa64251bc5e14879384ba83543b0849783e16
ivan <ig54902>
2025-01-11 17:22:34 +0100

    promijenjen pregled kategorija

--- frontend/src/components/HeaderDropDownMenu.tsx
+++ frontend/src/components/HeaderDropDownMenu.tsx
@@ -6,19 +6,11 @@ import { Link } from "react-router";
 const items: MenuProps["items"] = [
   {
     key: "1",
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.antgroup.com"
-      >
-        1st menu item
-      </a>
-    ),
+    label: <Link to="/">Oglasi</Link>,
   },
   {
-    key: "3",
-    label: <Link to="">Kat 1</Link>,
+    key: "2",
+    label: <Link to="/tema">Teme</Link>,
   },
 ];
 
