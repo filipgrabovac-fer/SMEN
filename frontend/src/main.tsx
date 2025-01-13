9c9941281b0e903c04a8c7dd7a59f757ae1668b3
ivan <ig54902>
2025-01-13 02:42:07 +0100

    changed frontend folder structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import { appRoutes } from "./appRoutes.tsx";
 
-const router = createBrowserRouter([{ element: <App />, children: appRoutes }]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
